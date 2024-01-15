95e6d5c2296e4f73e65b60833fbaf8daabaa3cca
Medhin12 <medhin@360ground.com>
2024-01-15 23:40:58 +0300

    player commit

---
+++ components/Billboard.tsx
@@ -0,0 +1,22 @@
+import useBillboard from "@/hooks/useBillboard";
+import React from "react";
+
+const Billboard = () => {
+    const { data } = useBillboard();
+    return (
+        <div className="relative h-[56.25]">
+            <video
+            className=""
+            autoPlay
+            muted
+            loop
+            poster={data?.thumbnailUrl}
+            src={data?.videoUrl}
+            >
+
+            </video>
+        </div>
+    )
+}
+
+export default Billboard;
